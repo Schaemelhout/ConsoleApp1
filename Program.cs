f1f45606a2835eb162aa04bee6285632905ad9f2
Tim Maes <maestim@cronos.be>
2025-07-07 14:53:12 +0200

    Cleanup

--- Program.cs
+++ Program.cs
@@ -8,18 +8,13 @@ services.AddMediatR(configuration =>
 {
     configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
     
-    // Solution 1: Using marker interface (WORKS!)
     configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
     configuration.AddOpenBehavior(typeof(QueryValidationBehavior<,>));
-    
-    // Solution 2: QueryResultValidationBehavior conflicts with LoggingBehavior
-    // configuration.AddOpenBehavior(typeof(QueryResultValidationBehavior<,>));
 });
 
 var serviceProvider = services.BuildServiceProvider();
 var mediator = serviceProvider.GetRequiredService<IMediator>();
 
-Console.WriteLine("Final test - Both behaviors working:");
 var result = await mediator.Send(new PingQuery());
 Console.WriteLine($"Result: {result.Succeeded}");
 Console.WriteLine($"Error: {result.Error}");
